56d954b5372cee2921f78b9c17cb1d9bce97db72
swetha <swetha1910@gmail.com>
2015-05-04 21:16:57 -0500

    Edited the arguments in the run script (run_classification.sh)

--- run_classification.sh
+++ run_classification.sh
@@ -1,1 +1,1 @@
-python classify.py data/sampled_data_$1 data/relevant.txt $1 $2 $3
+python classify.py data/undersample_output/train_sampled_$1 data/undersample_output/test_sampled_$1 data/relevant.txt $1 $2 $3
